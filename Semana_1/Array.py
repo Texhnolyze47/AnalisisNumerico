0217dc7496661f9c2afcc2a9b996475ccccb6ec6
Texhnolyze <ichigobankai136@gmail.com>
2022-08-31 12:46:22 -0500

    datos por teclado

--- Semana_1/Array.py
+++ Semana_1/Array.py
@@ -2,7 +2,12 @@ import numpy as np
 
 a= np.matrix([0,1,2,3,4,5])
 b= np.matrix([0,1,2,3,4,5])
-
-
-
 print(a + b)
+
+# por defecto los valores por teclado de valor string
+# tenemos que convertirlo a tipo numerico
+c = int(input("Proporciona un numero "))
+
+print(c)
+
+print(c * (a + b))
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy
-
 
 
 def print_hi(name):
@@ -9,4 +7,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_hi('PyCharm')
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
