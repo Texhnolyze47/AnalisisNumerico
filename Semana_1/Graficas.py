ad5abc1de8d7fa620a1483be463550a311a7aa0e
Texhnolyze <ichigobankai136@gmail.com>
2022-08-31 13:40:13 -0500

    graficas con diferentes expresiones

--- Semana_1/Graficas.py
+++ Semana_1/Graficas.py
@@ -1,10 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# Se pueden usar literales
+from sympy import *
+#
+x = Symbol("x")
 
-# y = x^2
-x = np.arange(-2, 2, .1)
-# se eleva a la segunda potencia
 y = x ** 2
 
-plt.plot(x, y)
+xx = np.arange(-2,2,.1)
+
+n = len(xx)
+# genera valores de arreglos con zeros
+yy = np.zeros(n)
+# la funcion subs evalua una sola variable
+# en caso de dos se usa evaluaf
+for i in range(n):
+    yy[i] = y.subs(x,xx[i])
+#
+plt.plot(xx,yy)
 plt.show()
+
+
+
+
