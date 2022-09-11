307753c2d6b7619a08d242e39b0e3d9a5a4d746e
Texhnolyze <ichigobankai136@gmail.com>
2022-09-10 22:19:10 -0500

    add: error porcentual y aproximado

--- Semana_2/homework/Tangente.py
+++ Semana_2/homework/Tangente.py
@@ -1,25 +1,22 @@
 import math as mt
 
-
 x = mt.pi / 4
+
+y = mt.tan(x)
+
+print("Valor real", y)
 
 seno = 0
 coseno = 0
 
-
 for n in range(0, 50):
-    seno += mt.pow(-1,n) / mt.factorial(2 * n + 1) * mt.pow(x, 2*n + 1)
-    coseno += pow(-1, n)* mt.pow(x,2 * n) / mt.factorial(2*n)
+    seno += mt.pow(-1, n) / mt.factorial(2 * n + 1) * mt.pow(x, 2 * n + 1)
+    coseno += pow(-1, n) * mt.pow(x, 2 * n) / mt.factorial(2 * n)
     tangente = seno / coseno
+
+    e = abs(tangente - y)
+    ep = abs((tangente - y) / y)
     print("valor aproximado ", tangente)
 
-
-
-
-
-
-
-
-
-
-
+    print("Error absoluto", e)
+    print("Error porcentual", ep)
