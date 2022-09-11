ce0620f32eeeacc05978e700984397f478651831
Texhnolyze <ichigobankai136@gmail.com>
2022-09-10 22:13:58 -0500

    fix typo

--- Semana_2/homework/Tangente.py
+++ Semana_2/homework/Tangente.py
@@ -8,7 +8,7 @@ coseno = 0
 
 
 for n in range(0, 50):
-    seno += math.pow(-1,n) / mt.factorial(2 * n + 1) * math.pow(x, 2*n + 1)
+    seno += mt.pow(-1,n) / mt.factorial(2 * n + 1) * mt.pow(x, 2*n + 1)
     coseno += pow(-1, n)* mt.pow(x,2 * n) / mt.factorial(2*n)
     tangente = seno / coseno
     print("valor aproximado ", tangente)
