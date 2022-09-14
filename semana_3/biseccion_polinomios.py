fd01eb641b0bc7517f87458389fb0d2ab01ddbfb
Texhnolyze <ichigobankai136@gmail.com>
2022-09-14 13:14:36 -0500

    add: evita que los programas de polinomios divida entre cero

--- semana_3/biseccion_polinomios.py
+++ semana_3/biseccion_polinomios.py
@@ -10,30 +10,31 @@ s = 0
 while s == 0:
     x0 = float(input("Proporcione un valor inicial"))
     x1 = float(input("Proporcione un valor final"))
-    if y.subs(x,x0) * y.subs(x,x1) < 0:
+    if y.subs(x, x0) * y.subs(x, x1) < 0:
         s = 1
     else:
         print("Los valores proporcionados son incorrentos")
 
-#x0 = 6
-#x1 = 20
+# x0 = 6
+# x1 = 20
 xr = 0
 er = 1
 # contador para saber que nivel de aproximacion puede
 i = 0
 
 while er > 1e-15:
-    xr = float(x1 - y.subs(x, x1) * (x1 - x0) / (y.subs(x, x1) - y.subs(x, x0)))
+    xr = (x0 + x1) / 2
     if y.subs(x, x1) * y.subs(x, xr) < 0:
         xr0 = x0
         x0 = xr
-
     else:
         xr0 = x1
         x1 = xr
+    # Esto evita que el programa divida entre 0
     if y.subs(x, xr) == 0:
         er = 0
     else:
         er = float(abs((y.subs(x, xr) - y.subs(x, xr0)) / y.subs(x, xr)))
+    # La variable que mide el error
     i += 1
 print("La raiz del polinomio ", y, "es", xr, " ", i)
