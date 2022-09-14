116573400514a3b40e77ae6ab4f05e01c0f8051c
Texhnolyze <ichigobankai136@gmail.com>
2022-09-14 12:33:14 -0500

    mejora de algoritmo

--- Semana_2/homework/TangenteFactorial.py
+++ Semana_2/homework/TangenteFactorial.py
@@ -9,14 +9,13 @@ print("Valor real", y)
 seno = 0
 coseno = 0
 
-for n in range(0, 6):
-    seno += mt.pow(-1, n) / mt.factorial(2 * n + 1) * mt.pow(x, 2 * n + 1)
-    coseno += pow(-1, n) * mt.pow(x, 2 * n) / mt.factorial(2 * n)
+for n in range(0, 8):
+    seno = seno + x ** (2 * n + 1) / mt.factorial(2 * n + 1) * (-1) ** n
+    coseno = coseno + x ** (2 * n) / mt.factorial(2 * n) * (-1) ** n
     tangente = seno / coseno
 
     e = abs(tangente - y)
     ep = abs((tangente - y) / y)
-    print("valor aproximado ", tangente)
 
-    print("Error absoluto", e)
-    print("Error porcentual", ep)
+    print(f"El {n+1} termino tiene el valor aproximado {tangente}, "
+          f"con un error absoluto {e} y un error porcentual {ep}")
--- semana_3/polinomios.py
+++ semana_3/polinomios.py
@@ -3,16 +3,24 @@ import sympy as sp
 
 x = sp.Symbol("x")
 
-y = x ** 3 - 64
+y = x ** 3 - 125
 x0 = 1
-x1 = 5
-xr = 20
+x1 = 20
+xr = 0
+er = 1
+# contador para saber que nivel de aproximacion puede
+i = 0
 
-while(y.subs(x,xr) > 1e-15 ):
-    xr = x1 - y.subs(x, x1) * (x1 - x0) / (y.subs(x, x1) - y.subs(x, x0))
-    if y.subs(x, x1)* y.subs(x, xr) < 0:
+while er > 1e-15:
+    xr = float(x1 - y.subs(x, x1) * (x1 - x0) / (y.subs(x, x1) - y.subs(x, x0)))
+    if y.subs(x, x1) * y.subs(x, xr) < 0:
+        xr0 = x0
         x0 = xr
-    else:
-        x1 = xr
 
-print("La raiz del polinomio ", y , "es", xr)
+    else:
+        xr0 = x1
+        x1 = xr
+    # La variable que mide el error
+    er = float(abs((y.subs(x, xr) - y.subs(x, xr0)) / y.subs(x, xr)))
+    i += 1
+print("La raiz del polinomio ", y, "es", xr, " ", i)
